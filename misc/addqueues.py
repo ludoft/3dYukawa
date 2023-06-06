7f500cc21839dc8bbe7c969bb86b6b52b5b0c53a
Ludo FT <ludooft@gmail.com>
2023-06-06 13:40:43 +0100

    added hydra shenanigans

---
+++ misc/addqueues.py
@@ -0,0 +1,52 @@
+import subprocess
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging;
+import time
+import psutil
+from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Read the list of commands from a file
+filename = "./executeThese3dSRFC446.list"  # Update with your file name
+with open(filename, "r") as file:
+    commands = file.read().splitlines()
+
+commandsToDo = commands[1:50]
+print(len(commandsToDo))
+
+logging.debug(f"{str(datetime.now())}: Beginning ALL with RAM at {str(psutil.virtual_memory()[2])}")
+"""
+# Function to execute a command and print the execution time
+def execute_command(command):
+    start_time = time.time()
+    printcommand = command.split(" /tmp/")[0]
+    logging.debug(f"{str(datetime.now())}: START'{printcommand}' with RAM at {str(psutil.virtual_memory()[2])}")
+    process = subprocess.Popen(command, shell=True)
+    process.communicate()  # Wait for the command to finish
+    end_time = time.time()
+    execution_time = end_time - start_time
+    logging.debug(f"{str(datetime.now())}: DONE '{printcommand}' executed in {execution_time:.2f} seconds.")
+
+# Execute commands in parallel using 12 cores
+with ThreadPoolExecutor(max_workers=14) as executor:
+    # Submit commands for execution
+    futures = [executor.submit(execute_command, command) for command in commandsToDo]
+
+    # Wait for all commands to complete
+    for future in as_completed(futures):
+        future.result()
+"""
+
+
+bad_substrings = ["hlll", "hhll", "ghll", "gghl", "gggh"]
+for command in commandsToDo:
+    #subprocess.run(command,shell=True, check=True)
+    if any(substring in command for substring in bad_substrings):
+        commandToExecute = "addqueue -c '2 hours' -n 1 -m 5 -s -i -q bigmem -g tensor446 " + command
+    else:
+        commandToExecute = "addqueue -c '2 hours' -n 1 -m 2 -s -i -q long -g tensor446 " + command
+    print(commandToExecute)
+
+
+print("Done.")
